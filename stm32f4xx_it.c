624073b2cc21239dc3aca80ed4238ae169035c48
josiyuh <josiahhorn23@gmail.com>
2025-12-09 12:21:43 -0600

    Create stm32f4xx_it.c

---
+++ stm32f4xx_it.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include "stm32f4xx_it.h"
+#include "rtos.h"
+
+void NMI_Handler(void)      { while (1) {} }
+void HardFault_Handler(void){ while (1) {} }
+void MemManage_Handler(void){ while (1) {} }
+void BusFault_Handler(void) { while (1) {} }
+void UsageFault_Handler(void){ while (1) {} }
+
+void SVC_Handler(void)      {}
+void DebugMon_Handler(void) {}
+void PendSV_Handler(void)   {}
+
+void SysTick_Handler(void)
+{
+  HAL_IncTick();      // HAL timebase
+  RTOS_TickHandler(); // RTOS scheduler tick
+}
